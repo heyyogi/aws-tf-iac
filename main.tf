1738daa1df3a14dce06549361b6657b9d24fcbc3
heyyogi <103329304+heyyogi@users.noreply.github.com>
2026-02-03 16:43:29 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name = "my-key-pair"
+
+  tags = {
+    Name = "MyInstance"
+  
+}
